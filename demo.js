f9e54fac9a4329b19f9fc21ac9cddef6dd9e1e7e
atrist <atristmu@outlook.com>
2020-09-11 11:31:22 +0800

    leetcode-双数之和

--- demo.js
+++ demo.js
@@ -1,26 +1,17 @@
 /**
  * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead.
+ * @param {number} target
+ * @return {number[]}
  */
-var moveZeroes = function (nums) {
-
-    let zeroFlag = 0;
-    for (let currentPoint = 0; currentPoint < nums.length; currentPoint++) {
-
-        // 判断当前指针 元素是否 为 0
-        // 如果不为零
-        if (nums[currentPoint] !== 0) {
-            // 就跟 慢指针进行交换
-            let swap = nums[currentPoint];
-            nums[currentPoint] = nums[zeroFlag];
-            nums[zeroFlag] = swap;
-            zeroFlag++;
-        }
+var twoSum = function (nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const temp = target - nums[i];
+        const result = nums.indexOf(temp,i+1);
+        if(result !== -1) return [i,result];
     }
-    return nums;
-}
+};
 
-let test = [0, 3, 0, 1, 12];
-console.log(moveZeroes(test));
-test = [0, 0, 1];
-console.log(moveZeroes(test));
+
+let t = [3, 4, 2];
+console.log(twoSum(t, 6));
