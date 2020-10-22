681d0a3ad2ba0258e7174b7f5ea553e0af849ba4
atrist <atristmu@outlook.com>
2020-10-22 09:38:20 +0800

    测试 travis.yml

--- demo.js
+++ demo.js
@@ -1,17 +1,9 @@
 /**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
+ * @param {character[][]} board
+ * @return {boolean}
  */
-var twoSum = function (nums, target) {
-    const map = new Map();
-    for (let i = 0; i < nums.length; i++) {
-        const temp = target - nums[i];
-        const result = nums.indexOf(temp,i+1);
-        if(result !== -1) return [i,result];
-    }
+var isValidSudoku = function(board) {
+
+    // 三次遍历
+    
 };
-
-
-let t = [3, 4, 2];
-console.log(twoSum(t, 6));
